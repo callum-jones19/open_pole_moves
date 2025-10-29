c03a51f7317de19208eb795330e8dd86f4385921
Callum Jones <callum.o.jones+git@protonmail.com>
2025-10-29 14:11:03 +1100

    Add basic search bar functionality and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import { useMemo, useState } from "react";
 import Header from "./components/Header";
 import MoveCard from "./components/MoveCard"
-import { generateMoveDB } from "./data/moves"
+import { generateMoveDB, type PoleMove } from "./data/moves"
 
 function App() {
-  const moveData = generateMoveDB().concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB());
+  const moveData = useMemo(() => {
+    return generateMoveDB().concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB()).concat(generateMoveDB());
+  }, []);
   
+  const [filteredMoves, setFilteredMoves] = useState<PoleMove[] | null>(null);
+
   return (
     <>
       <div className="w-screen h-screen">
-        <Header />
-        <div className="w-full shrink flex flex-row flex-wrap gap-5 p-5">
-          {moveData.map((md, i) => <MoveCard move={md} key={i} />)}  
+        <Header
+          onSearchString={searchedString => {
+            if (searchedString === "") {
+              setFilteredMoves(null);
+            } else {
+              const newFilteredMoves = moveData.filter(move => move.name.toLowerCase().includes(searchedString.toLowerCase()));
+              setFilteredMoves(newFilteredMoves);
+            }
+          }}
+        />
+        <div className="w-full shrink flex flex-row flex-wrap gap-5 p-5 pt-20">
+          {/* {moveData.map((md, i) => <MoveCard move={md} key={i} />)} */}
+          {filteredMoves === null ?
+            moveData.map((md, i) => <MoveCard move={md} key={i} />) :
+            filteredMoves.map((md, i) => <MoveCard move={md} key={i} />)
+          }
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export interface HeaderProps {
   onSearchString: (searchTerm: string) => void;
 }
 
-const Header = () => {
+const Header = ({ onSearchString }: HeaderProps) => {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
   
   const handleKeyDown = (e: KeyboardEvent) => {
@@ -29,9 +29,10 @@ const Header = () => {
 
   return (
     <>
-      <div className="w-full h-14 sticky top-0 bg-neutral-800 text-white flex flex-row justify-between">
+      <div className="w-full h-14 fixed top-0 bg-neutral-800 text-white flex flex-row justify-between">
         <div className="h-full basis-full flex-row sm:flex hidden">
           <button className="px-3 border-b-4 border-neutral-400 bg-neutral-900 p-1">Moves</button>
+          <button className="px-3 border-neutral-400 bg-neutral-800 p-1 hover:bg-neutral-700">Combos</button>
         </div>
         <button
           type="button"
@@ -41,15 +42,17 @@ const Header = () => {
           <Menu />
         </button>
         <input
-          className="m-2 rounded-md px-3 border-neutral-500 border" placeholder="Press 's' to search..."
+          className="m-2 rounded-md px-3 border-neutral-500 border w-96" placeholder="Press 's' to search..."
           id="headerSearchBar"
+          onChange={e => onSearchString(e.currentTarget.value)}
         />
       </div>
-      <div className={"z-20 w-26 h-full bg-neutral-800 text-white left-0 top-0 flex flex-col " + (showSidebar ? "sm:hidden absolute" : "hidden")}>
+      <div className={"z-20 w-26 h-full bg-neutral-800 text-white left-0 top-0 flex flex-col " + (showSidebar ? "sm:hidden fixed" : "hidden")}>
         <button className="p-2 hover:bg-neutral-600 h-14" onClick={() => setShowSidebar(false)}>
           <ArrowLeft />
         </button>
         <button className="bg-neutral-900 py-3 px-1 text-start">Moves</button>
+        <button className="bg-neutral-900 py-3 px-1 text-start">Combos</button>
       </div>
     </>
   )
